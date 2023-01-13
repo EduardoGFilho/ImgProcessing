83dfdea0054124e1867a47d1afdb6aa5acdcaff3
EduardoFilho <eduardo.filho@itec.ufpa.br>
2023-01-13 09:56:59 -0300

    Now FindPSNR outputs to a file

--- CreatingFolders/FindPSNR.py
+++ CreatingFolders/FindPSNR.py
@@ -6,16 +6,29 @@ import pathlib
 
 files = list(pathlib.Path("./Original").rglob("*.png"))
 
+txt = open("PSNR.txt","w")
+
 for i in files:
     original = cv2.imread(str(i))
+
+    txt.write(i.name)
+    txt.write("\n")
 
     noisy = list(pathlib.Path("./Noisy/" + i.stem).rglob("*.png"))
 
     for j in noisy:
+
         im_noisy = cv2.imread(str(j))
         v_PSNR = cv2.PSNR(original, im_noisy)
         print(j.name)
         print(v_PSNR)
+        txt.write("\t")
+        txt.write(j.name)
+        txt.write(": ")        
+        txt.write(str(v_PSNR))
+        txt.write("\n")
+
+
 
     filtered = list(pathlib.Path("./Filtered/" + i.stem).rglob("*.png"))
     for j in filtered:
@@ -23,4 +36,9 @@ for i in files:
         v_PSNR = cv2.PSNR(original, im_filtered)
         print(j.name)
         print(v_PSNR)
+        txt.write("\t")
+        txt.write(j.name)
+        txt.write(": ")        
+        txt.write(str(v_PSNR))
+        txt.write("\n")
     
